72cb5219ab1ef24b5883b4b58f69ab607b012747
eusorov <eusorov@googlemail.com>
2018-09-06 21:43:20 +0200

    log if allowed to check

--- strategies/RES_SUP.js
+++ strategies/RES_SUP.js
@@ -47,10 +47,10 @@ method.init = function() {
 
   this.addIndicator('smaMiddle20', 'SMA', this.settings.smaMiddle20.parameters.optInTimePeriod);
 
-  // always calculate daily sma 
+  // always calculate daily sma
   // 24h =  1440; 1440/240 = 6
   let factor = 1440 / this.tradingAdvisor.candleSize;
-  
+
   //TODO evtl. 140 +-10!
   this.smaDailies = [20,60,100,140,180,220,260]; //
   this.smaDailies.forEach(v => {
@@ -63,6 +63,7 @@ method.init = function() {
 
 // what happens on every new candle?
 method.update = function(candle) {
+  log.debug("requiredHistory:" + this.requiredHistory + " age:" +this.age);
 }
 
 // for debugging purposes: log the last calculated
@@ -90,22 +91,22 @@ method.check = function(candle) {
   this.currentValue = currentValue;
 
   this.prevValues.push(currentValue);
-  
+
   if (this.prevValues.length > 10) {
     this.prevValues.shift();
   }
-  
-  
+
+
   this.buyingAge = this.buyingAge > 0? this.buyingAge+1 : 0;
   //sell when sma broken
   //1. candle close under sma
-  this.breakSma = 0;  
+  this.breakSma = 0;
   if ((!this.bearMarket && this.hasBoughtBull) || (this.bearMarket && this.hasBoughtBear)){
     const smaDailies = this.bearMarket ? this.smaDailies.reverse() : this.smaDailies;
     smaDailies.some((v)=> {
-      const smaDaily = this['smaMiddle'+v+'daily'];                                         
-      if (breakSmaFn(this.breakSmaProcent, currentValue.candle, smaDaily, this.bearMarket, (this.buysma === v) 
-                // sell if eg MA20 breaks with one of three prevCandle > MA20 
+      const smaDaily = this['smaMiddle'+v+'daily'];
+      if (breakSmaFn(this.breakSmaProcent, currentValue.candle, smaDaily, this.bearMarket, (this.buysma === v)
+                // sell if eg MA20 breaks with one of three prevCandle > MA20
                 || (!this.bearMarket && this.buysma > v &&  this.prevValues.slice(-5).some((prevValue) => prevValue.candle.close > smaDaily ))
                 || ( this.bearMarket && this.buysma < v &&  this.prevValues.slice(-5).some((prevValue) => prevValue.candle.close < smaDaily ))
           )){
@@ -113,7 +114,7 @@ method.check = function(candle) {
         if (!this.bearMarket && (currentValue.candle.close < this['smaMiddle'+this.settings.res_sup.breakSmaBearMarket+'daily'])){ //
           this.bearMarket = true; //break MA then bearMarket
           console.log('set bearMarket = true');
-        }else if (this.bearMarket && (currentValue.candle.close >  this['smaMiddle'+this.settings.res_sup.breakSmaBullMarket+'daily'])){ 
+        }else if (this.bearMarket && (currentValue.candle.close >  this['smaMiddle'+this.settings.res_sup.breakSmaBullMarket+'daily'])){
           //XBT 140 vs 140 3%breakSma ETH bull 100 vs bear 180 6%breakSMA XRP 140 vs 140
           this.bearMarket = false;
           console.log('set bearMarket = false');
@@ -122,23 +123,23 @@ method.check = function(candle) {
       }
     });
   }
-  
+
 
 
   log.debug (candle.start.format('YYYY-MM-DD HH:mm')
    + ' currCandle: '+candle.close.toFixed(2)
-   + ' std: '+(this.indicators.smaMiddle140daily ? (this.indicators.smaMiddle140daily.std * 0.1).toFixed(2) : '') 
-   
+   + ' std: '+(this.indicators.smaMiddle140daily ? (this.indicators.smaMiddle140daily.std * 0.1).toFixed(2) : '')
+
   );
 }
 
-// we can make hier a stoploss? 
+// we can make hier a stoploss?
 method.candleOneMin = function(candle) {
   const isAllowedToCheck = this.requiredHistory <= this.age;
   if (!isAllowedToCheck){
     return;
   }
-  
+
   let isTrend = this.currentValue.aroonosc >= 50 || this.currentValue.aroonosc <= -50;
 
   //near smaLine within x%
@@ -147,26 +148,26 @@ method.candleOneMin = function(candle) {
 
   //sell
   //let cciChangedSell = cciChangedSellFn(currentValue.cci, prevValue);
-  
+
   //sell when we have in previous values max STC that is over thresholds.up
   // let maxPrevStc =this.prevValues.length > 1 ? Math.max(this.prevValues.slice(0, -1).map(v => v.stc)) : 0;
   // let stcSellSignal = (currentValue.stc <= this.settings.stc.thresholds.down && maxPrevStc >= this.settings.stc.thresholds.up);
 
   // if (!this.hasBoughtBull && candle.start > moment("2017-07-16 10:00:00") && candle.start < moment("2017-07-16 15:00:00")){
 
-    // log.debug(candle.start.format('YYYY-MM-DD HH:mm') 
-    // + ' prevCandle: '+(prevValue ? prevValue.candle.close.toFixed(2) : '') 
+    // log.debug(candle.start.format('YYYY-MM-DD HH:mm')
+    // + ' prevCandle: '+(prevValue ? prevValue.candle.close.toFixed(2) : '')
     // + ' curCandle:' +currentValue.candle.close.toFixed(2) + ' currMin:'+candle.close.toFixed(2)
     // + ' sma140:' +this.smaMiddle140daily.toFixed(2)
     // + ' sma180:' +this.smaMiddle180daily.toFixed(2)
     // + ' nearSma: '+nearSma ) ;
   // }
-  
 
-  let buyadviceProp = {nearSma : nearSma, isTrend: isTrend, bearMarket: this.bearMarket}  
-  //if (this.bearMarket && nearSma > 0) console.log('bear Market should buy bear!') 
 
-  let selladviceProp = {breakSma: this.breakSma, bearMarket: this.bearMarket, resistanceSma: resistanceSma}  
+  let buyadviceProp = {nearSma : nearSma, isTrend: isTrend, bearMarket: this.bearMarket}
+  //if (this.bearMarket && nearSma > 0) console.log('bear Market should buy bear!')
+
+  let selladviceProp = {breakSma: this.breakSma, bearMarket: this.bearMarket, resistanceSma: resistanceSma}
   // buy bull trend
   if (this.hasBoughtBull || !this.bearMarket){
     this.bullTrendStrat(candle, buyadviceProp, selladviceProp);
@@ -179,12 +180,12 @@ method.candleOneMin = function(candle) {
   // buy range
 
   //save prev
-  this.prevCandleOneMin = candle; 
+  this.prevCandleOneMin = candle;
 }
 
 method.bullTrendStrat = function(candle, buyadviceProp, selladviceProp){
 
-  if (!this.hasBoughtBull 
+  if (!this.hasBoughtBull
     //&& !this.skipCandle
     && ( buyadviceProp.nearSma > 0 )
     && buyadviceProp.isTrend
@@ -200,11 +201,11 @@ method.bullTrendStrat = function(candle, buyadviceProp, selladviceProp){
     // this.buyprice = candle.close;
     // this.pullStopOnce = false;
   }else if (this.hasBoughtBull
-    && (selladviceProp.breakSma > 0 
+    && (selladviceProp.breakSma > 0
       // || selladviceProp.resistanceSma > 0
       // || (candle.close < this.stop)
       ) //|| this.buyingAge >= 10
-    
+
   ){
     this.hasBoughtBull = false;
     this.advice('short', candle, selladviceProp);
@@ -219,7 +220,7 @@ method.bullTrendStrat = function(candle, buyadviceProp, selladviceProp){
 
 method.bearTrendStrat = function(candle, buyadviceProp, selladviceProp){
 
-  if (!this.hasBoughtBear 
+  if (!this.hasBoughtBear
     //&& !this.skipCandle
     && ( buyadviceProp.nearSma > 0 )
     && buyadviceProp.isTrend
@@ -235,11 +236,11 @@ method.bearTrendStrat = function(candle, buyadviceProp, selladviceProp){
     // this.buyprice = candle.close;
     // this.pullStopOnce = false;
   }else if (this.hasBoughtBear
-    && (selladviceProp.breakSma > 0 
+    && (selladviceProp.breakSma > 0
       //  || selladviceProp.resistanceSma > 0
       //  || (candle.close > this.stop)
       ) //|| this.buyingAge >= 10
-    
+
   ){
     this.hasBoughtBear = false;
     this.advice('long bear', candle, selladviceProp);
@@ -258,7 +259,7 @@ function getResistanceSma(candle, smaDailies, indicators, buysma, bearMarket, sh
     smaDailies = bearMarket ? smaDailies.reverse() : smaDailies;
     smaDailies.some((v) => {
       const smaDaily = indicators['smaMiddle' + v + 'daily'];
-      //2 sell when we are close to another sma and 
+      //2 sell when we are close to another sma and
       // check only smaller SMA, only if smaller SMA above buySMA, only if smaller SMA > next SMA
       if (resistanceSmaFn(1, candle, smaDaily, bearMarket, (!bearMarket && (buysma > v)) || (bearMarket && (buysma < v)) )) {
         resistanceSma = v;
@@ -277,14 +278,14 @@ function getNearSma(candle, prevValue, smaDailies, indicators, procent, bearMark
       // if ((!bearMarket && v < 100) || (bearMarket && v >180)) return false; // no buy at upper bands or lower bands
       const smaNext = !bearMarket ? v - 40 : v+40;
       const smaDaily = indicators['smaMiddle' + v + 'daily'];
-      const smaNextDaily = indicators['smaMiddle' + smaNext + 'daily'];             
-      if (nearSmaFn(procent, candle, smaDaily, prevValue, bearMarket, (!bearMarket && smaDaily < smaNextDaily) || (bearMarket)  
+      const smaNextDaily = indicators['smaMiddle' + smaNext + 'daily'];
+      if (nearSmaFn(procent, candle, smaDaily, prevValue, bearMarket, (!bearMarket && smaDaily < smaNextDaily) || (bearMarket)
           // with reverse with need check MA20 explicitly
           || (!bearMarket && v=== 20 && smaDaily  > indicators['smaMiddle' + (v+40) + 'daily'])
           || ( bearMarket && v=== 220 && smaDaily < indicators['smaMiddle' + (v-40) + 'daily'])
         )) {
         nearSma = v;
-        //if (bearMarket) console.log('bear Market should buy bear!' +nearSma) 
+        //if (bearMarket) console.log('bear Market should buy bear!' +nearSma)
         return true;
       }
     });
@@ -296,7 +297,7 @@ function nearSmaFn (procent, candle, sma, prevValue, bearMarket, shouldCheck){
   let nearSma = false;
   if (!shouldCheck) return false;
 
-  
+
   if (!bearMarket){
     // if the momentum (roc too strong, adjust we buy below sma) ??
     if (candle.close * (1-procent*0.01)<=sma && candle.close >= sma){
@@ -322,10 +323,10 @@ function nearSmaFn (procent, candle, sma, prevValue, bearMarket, shouldCheck){
 function breakSmaFn (procent, candle, sma, bearMarket, shouldCheck){
   let breakSma = false;
   if (!shouldCheck)  return false;
-  
+
   //if (bearMarket) console.log('bearMarket breakSMA shouldCheck')
 
-  if ((!bearMarket && candle.close * (1+(procent+0)*0.01) < sma) || (bearMarket && candle.close * (1-(procent+0)*0.01) > sma)){ 
+  if ((!bearMarket && candle.close * (1+(procent+0)*0.01) < sma) || (bearMarket && candle.close * (1-(procent+0)*0.01) > sma)){
     breakSma = true;
     //if (bearMarket) console.log('bearMarket breakSMA')
   }
@@ -338,11 +339,11 @@ function resistanceSmaFn (procent, candle, sma, bearMarket, shouldCheck){
   if (!shouldCheck)
     return false;
 
-  if (!bearMarket && candle.close * (1+procent*0.01) > sma 
+  if (!bearMarket && candle.close * (1+procent*0.01) > sma
    && candle.close * (1+procent*0.01) < (sma * (1+(procent+2)*0.01))){ // not more than 5 procent over
     resistanceSma = true;
-  }else 
-  if (bearMarket && candle.close * (1-procent*0.01) < sma 
+  }else
+  if (bearMarket && candle.close * (1-procent*0.01) < sma
    && candle.close * (1-procent*0.01) > (sma * (1-(procent+2)*0.01))){ // not more than 5 procent over
     resistanceSma = true;
   }
@@ -352,13 +353,13 @@ function resistanceSmaFn (procent, candle, sma, bearMarket, shouldCheck){
 
 var cciChangedSellFn = function(currentCci, prevValue){
   let prevCci = prevValue ? prevValue.cci : undefined;
-  return  prevCci > 70 && currentCci < -70 
+  return  prevCci > 70 && currentCci < -70
 }
-  
+
 var crossSmaWithMomentum = function(currentRoc, crossSmaDaysAgo ){
   const cutMiddleWithMomentum = (crossSmaDaysAgo > 0 && crossSmaDaysAgo <= this.settings.roc.thresholds.cross_in_last_days
     && (currentRoc <= this.settings.roc.thresholds.down))
-  
+
   return cutMiddleWithMomentum;
 }
 module.exports = method;
